efea179402b113672ff964ab85190927142f9703
NastyaShon <web.orlova.dev@gmail.com>
2023-09-16 12:11:51 +0300

    Разрешение текстовых сообщений, /start, /clean

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,58 @@ import TelegramBot from 'node-telegram-bot-api'; // библиотека для 
 import { config } from './config';
 import { prisma } from './prisma';
 
-prisma.message.findMany({}).then(console.log).catch(console.error)
 
-// const bot = new TelegramBot(config.BOT_TOKEN, { polling: true });
-// // создание бота(с нашим токеном и разрешение на получение сообщений)
+const bot = new TelegramBot(config.BOT_TOKEN, { polling: true });
+// создание бота(с нашим токеном и разрешение на получение сообщений)
 
-// bot.on('message', async msg => { // метод прослушивание входяших сообщений
-//   if (msg.from?.id !== config.CHES_TG_ID && msg.from?.id !== config.SHON_TG_ID) {
-//     bot.sendMessage(msg.chat.id, 'You are not allowed to use this bot! Ты бука)');
-//     return; // завершает функцию если условие выше выполнилось
-//   }
-//   // msg.from.id - тот кто отправил сообщение 
+bot.on('message', async msg => { // метод прослушивание входяших сообщений
+  if (msg.from?.id !== config.CHES_TG_ID && msg.from?.id !== config.SHON_TG_ID) {
+    bot.sendMessage(msg.chat.id, 'You are not allowed to use this bot!');
+    return; 
+  }
+  
+  if (msg.text === "/start") {
+    bot.sendMessage(msg.chat.id, "Привет, я бот, который сохраняет ваши сообщения");
+    return;
+  }
 
-//   bot.sendMessage(msg.chat.id, 'Hello World!'); // Отправляет сообщение
-// });
+  if (msg.text === "/clean") {
+    try {
+        await prisma.message.deleteMany({
+            where: {
+                content: {
+                    not: '/start'
+                }
+            }
+        });
+
+        bot.sendMessage(msg.chat.id, "Bce сообщения были удалены из базы данных. Пожалуйста, очистите историю чата вручную.");
+        return;
+    } catch(error) {
+        console.error("Ошибка при очистке сообщений:", error);
+        bot.sendMessage(msg.chat.id, "Произошла ошибка при очистке сообщений.");
+        return;
+    }
+  }
+
+  if(msg.text) {
+    try {
+        await prisma.message.create({
+            data: {
+                content: msg.text,
+                timestamp: new Date()
+            }
+        });
+
+        bot.sendMessage(msg.chat.id, `Вы написали: "${msg.text}". Я это сохранил!`);
+    } catch(error) {
+        bot.sendMessage(msg.chat.id, "Произошла ошибка при сохранении сообщения.");
+        console.error(error);
+    }
+  } else {
+    bot.sendMessage(msg.chat.id, 'Вы можете отправлять только текст');
+  }
+});
 
 
 
