8873469803eb6cd5cd3eb1e45151f8f1eebfee8b
Slava <slvrlv8@gmail.com>
2023-10-26 11:46:09 -0700

    DB working version 2 collections

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import TelegramBot from "node-telegram-bot-api";
-import { db } from "./db";
+import TelegramBot from 'node-telegram-bot-api';
 
 const BOT_TOKEN = '6970020467:AAEzqlR2eTwnKc0--LtfhXgalAAqUoU4uM4';
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-bot.on('message',async msg => {
-    console.log(msg)
-})
+bot.on('message', async msg => {
+  console.log(msg);
+});
